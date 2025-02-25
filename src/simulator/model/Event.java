dd5e2891ddb6ebad598750452b51dd86cd925286
jaimelhd2 <jaamaya@ucm.es>
2025-02-25 19:37:54 +0100

    25/02
    NewVehicleEvent terminado, test SIN pasar

---
+++ src/simulator/model/Event.java
@@ -0,0 +1,37 @@
+package simulator.model;
+
+public abstract class Event implements Comparable<Event> {
+
+  private static long _counter = 0;
+
+  protected int _time;
+  protected long _time_stamp;
+
+  Event(int time) {
+    if ( time < 1 )
+      throw new IllegalArgumentException("Invalid time: "+time);
+    else {
+      _time = time;
+      _time_stamp = _counter++;
+    }
+  }
+
+  int getTime() {
+    return _time;
+  }
+
+  @Override
+  public int compareTo(Event o) {
+    // TODO complete the method to compare events according to their _time, and when
+// _time is equal it compares the _time_stamp;
+	  if(this._time > o._time) return 1;
+	  if(this._time < o._time) return -1;
+	  else {
+		  if(this._time_stamp < o._time_stamp) return 1;
+		  if(this._time_stamp > o._time_stamp) return -1;
+		  else return 0;
+	  }
+  }
+
+  abstract void execute(RoadMap map);
+}
---
+++ src/simulator/model/NewVehicleEvent.java
@@ -0,0 +1,34 @@
+package simulator.model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class NewVehicleEvent extends Event{
+	private String _id;
+	private int _maxSpeed;
+	private int _contClass;
+	private List<String> _itinerary;
+	
+	
+	public NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) {
+	    super(time);
+	    this._id = id;
+	    this._maxSpeed = maxSpeed;
+	    this._contClass = contClass;	    
+	}
+
+	@Override
+	void execute(RoadMap map) {
+		List<Junction> parse = new ArrayList<Junction>();
+		Iterator<String> it = this._itinerary.iterator();
+		while(it.hasNext()) {
+			parse.add(map.getJunction(it.next()));
+		}
+		Vehicle v = new Vehicle(this._id, this._maxSpeed, this._contClass, parse);
+		map.addVehicle(v);
+		v.moveToNextRoad();
+		// TODO Auto-generated method stub
+		
+	}
+}
