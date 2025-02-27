39d055cb30747ceceff76bedaf9641ebd7b30b39
mrcszdz <caguai680@gmail.com>
2025-02-27 23:56:29 +0100

    Controller
    Falta main

---
+++ src/simulator/control/Controller.java
@@ -0,0 +1,50 @@
+package simulator.control;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import simulator.factories.Factory;
+import simulator.model.Event;
+import simulator.model.TrafficSimulator;
+
+public class Controller {
+    private TrafficSimulator _sim;
+    private Factory<Event> _eventsFactory;
+
+    public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
+        _sim = sim;
+        _eventsFactory = eventsFactory;
+    }
+    
+    public void loadEvents(InputStream in) {
+    	//falta la excepcion q nipu de como hacerla
+    	JSONObject jo = new JSONObject(new JSONTokener(in));
+    	JSONArray ja = jo.getJSONArray("events");
+    	for(int i = 0; i < ja.length(); i++) {
+    		this._sim.addEvent(this._eventsFactory.create_instance(ja.getJSONObject(i)));
+    	}
+    }
+    
+    public void run(int n, OutputStream out) {
+    	PrintStream p = new PrintStream(out);
+    	JSONObject jo = new JSONObject();
+    	JSONArray ja = new JSONArray();
+    	
+    	for (int i = 0; i < n; i++) {
+    	    _sim.advance();
+    	    ja.put(_sim.report());
+    	}
+		jo.put("states", ja);
+		p.print(jo);
+    }
+    
+    public void reset() {
+    	this._sim.reset();
+    }
+    
+}
